f60d7888f413efe9042d0e5e6861df44b0d65707
Denis <candyboobers@gmail.com>
2025-10-13 21:26:42 +0200

    Get back missing quotes

--- simulator/load.js
+++ simulator/load.js
@@ -33,15 +33,15 @@ function randomString(length) {
 }
 
 function generateLogField() {
-    let log = ts=${new Date().toISOString()};
+    let log = ts=`${new Date().toISOString()}`;
     for (let i = 0; i < 24; i++) {
-        log +=  field${i}=${randomString(10)};
+        log += `field${i}=${randomString(10)}`;
     }
     return log;
 }
 
 function uuid() {
-    return ${randomString(8)}-${randomString(4)}-${randomString(4)}-${randomString(4)}-${randomString(12)};
+    return `${randomString(8)}-${randomString(4)}-${randomString(4)}-${randomString(4)}-${randomString(12)}`;
 }
 
 export default function () {
@@ -53,26 +53,26 @@ export default function () {
         payload.push({
             "kubernetes.annotations.kubectl.kubernetes.io/default-container": "prometheus",
             "kubernetes.annotations.kubernetes.io/psp": "eks.privileged",
-            "kubernetes.container_hash": quay.io/prometheus/prometheus@sha256:${randomString(64)},
+            "kubernetes.container_hash": `quay.io/prometheus/prometheus@sha256:${randomString(64)}`,
             "kubernetes.container_image": "quay.io/prometheus/prometheus:v2.39.1",
             "kubernetes.container_name": "prometheus",
             "kubernetes.docker_id": randomString(64),
-            "kubernetes.host": ip-10-2-50-${Math.floor(Math.random() * 256)}.us-east-2.compute.internal,
+            "kubernetes.host": `ip-10-2-50-${Math.floor(Math.random() * 256)}.us-east-2.compute.internal`,
             "kubernetes.labels.app.kubernetes.io/component": "prometheus",
-            "kubernetes.labels.app.kubernetes.io/instance": k8s-${uniqueId},
+            "kubernetes.labels.app.kubernetes.io/instance": `k8s-${uniqueId}`,
             "kubernetes.labels.app.kubernetes.io/managed-by": "prometheus-operator",
             "kubernetes.labels.app.kubernetes.io/name": "prometheus",
             "kubernetes.labels.app.kubernetes.io/part-of": "kube-prometheus",
             "kubernetes.labels.app.kubernetes.io/version": "2.39.1",
-            "kubernetes.labels.controller-revision-hash": prometheus-k8s-${randomString(10)},
+            "kubernetes.labels.controller-revision-hash": `prometheus-k8s-${randomString(10)}`,
             "kubernetes.labels.operator.prometheus.io/name": "k8s",
             "kubernetes.labels.operator.prometheus.io/shard": "0",
             "kubernetes.labels.prometheus": "k8s",
-            "kubernetes.labels.statefulset.kubernetes.io/pod-name": prometheus-k8s-${uniqueId},
+            "kubernetes.labels.statefulset.kubernetes.io/pod-name": `prometheus-k8s-${uniqueId}`,
             "kubernetes.namespace_name": "monitoring",
-            "kubernetes.pod_id": uuid(),
-            "kubernetes.pod_name": prometheus-k8s-${uniqueId},
-            "log": generateLogField(),
+            "kubernetes.pod_id": `uuid()`,
+            "kubernetes.pod_name": `prometheus-k8s-${uniqueId}`,
+            "log": `generateLogField()`,
             "stream": "stderr"
         })
     }
